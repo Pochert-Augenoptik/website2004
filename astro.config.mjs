d8490a279f76007217613f7b19ac0aeb7ab5cf38
Florian Polster <florian.polster@staffbase.com>
2026-04-25 22:14:41 +0200

    Adopt Astro fontProvider

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 import mdx from "@astrojs/mdx";
 
 export default defineConfig({
   integrations: [mdx()],
+  fonts: [
+    {
+      provider: fontProviders.bunny(),
+      name: "Alan Sans",
+      cssVariable: "--font-alan-sans",
+      weights: ["100 900"],
+      styles: ["normal"],
+    },
+  ],
 });
